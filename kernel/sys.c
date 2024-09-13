c3e4f024da179bba2929e941550901e03cf78cfe
Tim Zimmermann <tim@linux4.de>
2024-09-13 17:51:55 +0200

    kernel: bpf: Pretend to support ANY prog_type/attach_type
    * Also guard the entire workaround behind a config
    
    Change-Id: Iae454ae98c54fb02f51f07a9683b9814a97b19a3

--- kernel/sys.c
+++ kernel/sys.c
@@ -1196,18 +1196,19 @@ SYSCALL_DEFINE1(newuname, struct new_utsname __user *, name)
 	down_read(&uts_sem);
 	memcpy(&tmp, utsname(), sizeof(tmp));
 	if (!is_legacy_ebpf) {
-	  if (!strncmp(current->comm, "bpfloader", 9) ||
-	      !strncmp(current->comm, "netbpfload", 10) ||
-	      !strncmp(current->comm, "netd", 4)) {
-	    strcpy(tmp.release, "5.4.0");
-	    pr_debug("fake uname: %s/%d release=%s\n",
-		     current->comm, current->pid, tmp.release);
-	  }
-	}
-
 #ifdef CONFIG_KSU_SUSFS_SPOOF_UNAME
 	susfs_spoof_uname(&tmp);
 #endif
+#ifdef CONFIG_ANDROID_SPOOF_KERNEL_VERSION_FOR_BPF
+	if (!strncmp(current->comm, "bpfloader", 9) ||
+	    !strncmp(current->comm, "netbpfload", 10) ||
+	    !strncmp(current->comm, "netd", 4)) {
+		 strcpy(tmp.release, "5.4.186");
+		 pr_debug("fake uname: %s/%d release=%s\n",
+		 current->comm, current->pid, tmp.release);
+	}
+#endif
+      }
 	up_read(&uts_sem);
 	if (copy_to_user(name, &tmp, sizeof(tmp)))
 		return -EFAULT;
